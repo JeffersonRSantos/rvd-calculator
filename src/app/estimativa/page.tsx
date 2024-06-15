4f9903f736369155ad02c19d51160504e2916744
Jefferson Santos <jefferson.santos@estadao.com>
2024-06-14 21:52:40 -0300

    feat: funcionalida de rota, menu clicavel

---
+++ src/app/estimativa/page.tsx
@@ -0,0 +1,5 @@
+export default function Estimativa(){
+    return(
+        <p className="text-center text-lg px-6 text-white"><strong>/estimativa</strong> - Em desenvolvimento</p>
+    )
+}
---
+++ src/app/tradepack/page.tsx
@@ -0,0 +1,5 @@
+export default function Tradepack(){
+    return(
+        <p className="text-center text-lg px-6 text-white"><strong>/tradepack</strong> - Em desenvolvimento</p>
+    )
+}
